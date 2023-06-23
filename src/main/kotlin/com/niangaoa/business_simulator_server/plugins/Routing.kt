6a0588fad14adf49cd44bd16e47689ea63cf1a8a
niangaoa <niangaoa@hotmail.com>
2023-06-23 10:59:55 +0800

    Init Commit

---
+++ src/main/kotlin/com/niangaoa/business_simulator_server/plugins/Routing.kt
@@ -0,0 +1,9 @@
+package com.niangaoa.business_simulator_server.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+    routing {
+    }
+}
